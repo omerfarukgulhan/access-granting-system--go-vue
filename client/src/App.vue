6108f289c4cc892f48b379445cb22698ee5d5bf9
omerfarukgulhan <omerfarukgulhan@outlook.com>
2024-10-12 19:34:13 +0300

    client temporarily deleted(version conflict)

--- client/src/App.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <p>Initial</p>
-</template>
-
-<script>
-
-</script>
-
-<style scoped></style>
--- client/src/store/modules/users/mutations.js
+++
@@ -1,5 +0,0 @@
-export default {
-  setUsers(state, payload) {
-    state.users = payload;
-  },
-};
