91a6ed8651d98017f60f7d4ff95cdb720ef73a7b
omerfarukgulhan <omerfarukgulhan@outlook.com>
2024-10-12 17:46:07 +0300

    cors added in backend

--- server/main.go
+++ server/main.go
@@ -6,6 +6,7 @@ import (
 	"access-granting/controller"
 	"log"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -15,6 +16,17 @@ func main() {
 	postgresql.MigrateTables(db)
 
 	router := gin.Default()
+
+	router.Use(cors.Default())
+
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:5173"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+	}))
+
 	controller.InitializeRouter(db, router)
 	router.Static("/profile-images", "./uploads/profile-images")
 
