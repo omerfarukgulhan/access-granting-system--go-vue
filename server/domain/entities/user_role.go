7ede2e5a435e0e6e2f4a51994460a515a39e7a2c
omerfarukgulhan <omerfarukgulhan@outlook.com>
2024-10-16 21:36:58 +0300

    user role gets deleted when user or role is deleted

--- server/domain/entities/user_role.go
+++ server/domain/entities/user_role.go
@@ -3,7 +3,7 @@ package entities
 import "time"
 
 type UserRole struct {
-	UserId    int64     `gorm:"primaryKey"`
-	RoleId    int64     `gorm:"primaryKey"`
+	UserID    int64     `gorm:"primaryKey;constraint:OnDelete:CASCADE;"`
+	RoleID    int64     `gorm:"primaryKey;constraint:OnDelete:CASCADE;"`
 	CreatedAt time.Time `gorm:"autoCreateTime"`
 }
