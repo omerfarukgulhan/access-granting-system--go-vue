e20368fafab8b5710db8aa1e6ed78aeee5d869af
omerfarukgulhan <omerfarukgulhan@outlook.com>
2024-10-16 21:43:28 +0300

    soft delete on users changed to hard delete

--- server/domain/entities/user.go
+++ server/domain/entities/user.go
@@ -2,21 +2,18 @@ package entities
 
 import (
 	"time"
-
-	"gorm.io/gorm"
 )
 
 type User struct {
-	Id                 int64          `gorm:"primaryKey;autoIncrement"`
-	Username           string         `gorm:"size:255;not null;unique"`
-	Email              string         `gorm:"size:255;not null;unique"`
-	Password           string         `gorm:"size:255;not null"`
-	ProfileImage       string         `gorm:"size:255;default:'default.png'"`
-	IsActive           bool           `gorm:"default:false"`
-	ActivationToken    string         `gorm:"size:255"`
-	PasswordResetToken string         `gorm:"size:255"`
-	CreatedAt          time.Time      `gorm:"autoCreateTime"`
-	UpdatedAt          time.Time      `gorm:"autoUpdateTime"`
-	DeletedAt          gorm.DeletedAt `gorm:"index"`
-	Roles              []Role         `gorm:"many2many:user_roles"`
+	Id                 int64     `gorm:"primaryKey;autoIncrement"`
+	Username           string    `gorm:"size:255;not null;unique"`
+	Email              string    `gorm:"size:255;not null;unique"`
+	Password           string    `gorm:"size:255;not null"`
+	ProfileImage       string    `gorm:"size:255;default:'default.png'"`
+	IsActive           bool      `gorm:"default:false"`
+	ActivationToken    string    `gorm:"size:255"`
+	PasswordResetToken string    `gorm:"size:255"`
+	CreatedAt          time.Time `gorm:"autoCreateTime"`
+	UpdatedAt          time.Time `gorm:"autoUpdateTime"`
+	Roles              []Role    `gorm:"many2many:user_roles"`
 }
