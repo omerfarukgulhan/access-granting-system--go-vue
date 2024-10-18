2c4865a671b1f01990d24a590628f8287dc6c3b5
omerfarukgulhan <omerfarukgulhan@outlook.com>
2024-10-18 13:04:13 +0300

    login and registration pages added

--- client/src/pages/auth/LoginPage.vue
+++ client/src/pages/auth/LoginPage.vue
@@ -1,11 +1,76 @@
-<script setup>
-
-</script>
-
 <template>
-login
+  <div class="login-container">
+    <div class="box has-background-white">
+      <h1 class="title has-text-centered has-text-black">Login</h1>
+      <form @submit.prevent="handleLogin">
+        <div class="field">
+          <label class="label has-text-black" for="email">Email</label>
+          <div class="control">
+            <input
+                v-model="email"
+                id="email"
+                class="input"
+                placeholder="Email"
+                type="email"
+                required
+            />
+          </div>
+        </div>
+        <div class="field">
+          <label class="label has-text-black" for="password">Password</label>
+          <div class="control">
+            <input
+                v-model="password"
+                id="password"
+                class="input"
+                placeholder="Password"
+                type="password"
+                required
+            />
+          </div>
+        </div>
+        <div class="field">
+          <div class="control">
+            <button type="submit" class="button is-primary is-fullwidth">Login</button>
+          </div>
+        </div>
+        <p v-if="errorMessage" class="has-text-danger">{{ errorMessage }}</p>
+      </form>
+    </div>
+  </div>
 </template>
 
-<style scoped>
+<script>
+export default {
+  data() {
+    return {
+      email: 'omer@omer.com',
+      password: 'P4ssword',
+      errorMessage: '',
+    };
+  },
+  methods: {
+    async handleLogin() {
+      try {
+        await this.$store.dispatch('auth/login', {email: this.email, password: this.password});
+        this.$router.push('/');
+      } catch (error) {
+        this.errorMessage = error.message;
+      }
+    },
+  },
+};
+</script>
 
+<style scoped>
+.login-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 2rem;
+ }
+
+.box {
+  width: 300px;
+}
 </style>
--- client/src/pages/auth/RegisterPage.vue
+++ client/src/pages/auth/RegisterPage.vue
@@ -1,11 +1,95 @@
-<script setup>
-
-</script>
-
 <template>
-register
+  <div class="register-container">
+    <div class="box has-background-white">
+      <h1 class="title has-text-centered has-text-black">Register</h1>
+      <form @submit.prevent="handleRegister">
+        <div class="field">
+          <label class="label has-text-black" for="email">Email</label>
+          <div class="control">
+            <input
+                v-model="email"
+                id="email"
+                class="input"
+                placeholder="Email"
+                type="email"
+                required
+            />
+          </div>
+        </div>
+        <div class="field">
+          <label class="label has-text-black" for="username">Username</label>
+          <div class="control">
+            <input
+                v-model="username"
+                id="username"
+                class="input"
+                placeholder="Username"
+                type="text"
+                required
+            />
+          </div>
+        </div>
+        <div class="field">
+          <label class="label has-text-black" for="password">Password</label>
+          <div class="control">
+            <input
+                v-model="password"
+                id="password"
+                class="input"
+                placeholder="Password"
+                type="password"
+                required
+            />
+          </div>
+        </div>
+        <div class="field">
+          <div class="control">
+            <button type="submit" class="button is-primary is-fullwidth">Register</button>
+          </div>
+        </div>
+        <p v-if="errorMessage" class="has-text-danger">{{ errorMessage }}</p>
+      </form>
+    </div>
+  </div>
 </template>
 
-<style scoped>
+<script>
+export default {
+  data() {
+    return {
+      email: 'faruk@faruk.com',
+      username: 'username2',
+      password: 'P4ssword',
+      errorMessage: '',
+    };
+  },
+  methods: {
+    async handleRegister() {
+      try {
+        await this.$store.dispatch('auth/register', {
+          email: this.email,
+          username: this.username,
+          password: this.password
+        });
+        alert("activate email")
+        this.$router.push('/login');
+      } catch (error) {
+        this.errorMessage = error.message;
+      }
+    },
+  },
+};
+</script>
 
+<style scoped>
+.register-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 2rem;
+}
+
+.box {
+  width: 300px;
+}
 </style>
