2ebadc9a033f3c0afbd2c1b3b86899d96d36647b
omerfarukgulhan <omerfarukgulhan@outlook.com>
2024-10-19 00:21:46 +0300

    client routes secured

--- client/src/router.js
+++ client/src/router.js
@@ -8,6 +8,7 @@ import UserRoles from "@/pages/user-roles/UserRoles.vue";
 import LoginPage from "@/pages/auth/LoginPage.vue";
 import RegisterPage from "@/pages/auth/RegisterPage.vue";
 import UserProfile from "@/pages/users/UserProfile.vue";
+import store from "./store/index.js"
 
 const router = createRouter({
   history: createWebHistory(),
@@ -15,14 +16,26 @@ const router = createRouter({
     {path: '/', redirect: '/users'},
     {path: '/users', component: UsersList},
     {path: '/users/:id', component: UserInfo, props: true},
-    {path: '/profile', component: UserProfile, props: true},
-    {path: '/roles', component: RolesList},
-    {path: '/roles/:id', component: RoleInfo, props: true},
-    {path: '/user-roles', component: UserRoles},
-    {path: '/login', component: LoginPage},
-    {path: '/register', component: RegisterPage},
+    {path: '/profile', component: UserProfile, props: true, meta: {requiresAuth: true}},
+    {path: '/roles', component: RolesList, meta: {requiresAdmin: true}},
+    {path: '/roles/:id', component: RoleInfo, props: true, meta: {requiresAdmin: true}},
+    {path: '/user-roles', component: UserRoles, meta: {requiresAdmin: true}},
+    {path: '/login', component: LoginPage, meta: {requiresAuth: false}},
+    {path: '/register', component: RegisterPage, meta: {requiresAuth: false}},
     {path: "/:notFound(.*)", component: NotFound},
   ],
+});
+
+router.beforeEach((to, from, next) => {
+  const isAuthenticated = store.getters.isAuthenticated;
+  const isAdmin = store.getters.isAdmin;
+  if (to.meta.requiresAuth && !isAuthenticated) {
+    next('/login');
+  } else if (to.meta.requiresAdmin && !isAdmin) {
+    next('/');
+  } else {
+    next();
+  }
 });
 
 export default router;
