e8394c6d8d61d8dd22d5e31de53bcad4ede0d946
omerfarukgulhan <omerfarukgulhan@outlook.com>
2024-10-20 23:02:44 +0300

    adding user to role or adding role to user pages added

---
+++ client/src/pages/user-roles/AddRoleToUser.vue
@@ -0,0 +1,113 @@
+<template>
+  <div class="container">
+    <h2 class="title is-4">Add Role to User</h2>
+    <h4 class="subtitle">Select a role to add to the user</h4>
+    <h4>User Name: <strong>{{ user.username }}</strong></h4>
+    <h4>User ID: <strong>{{ user.id }}</strong></h4>
+
+    <div class="notification is-info" v-if="loading">
+      Loading roles...
+    </div>
+
+    <ul class="list">
+      <li v-for="role in roles" :key="role.id" class="m-4">
+        <button class="button is-primary box" @click="addRoleToUser(role.id)">
+          {{ role.name }} (ID: {{ role.id }})
+        </button>
+      </li>
+    </ul>
+
+    <div class="notification is-danger notification-width" v-if="error">{{ error }}</div>
+    <div class="notification is-success notification-width" v-if="successMessage">{{ successMessage }}</div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+const serverUrl = import.meta.env.VITE_SERVER_URL + "/user-roles";
+
+export default {
+  props: ['userId'],
+  data() {
+    return {
+      loading: false,
+      error: null,
+      successMessage: null
+    };
+  },
+  computed: {
+    roles() {
+      return this.$store.getters['roles/allRoles'];
+    },
+    user() {
+      return this.$store.getters['users/currentUser'];
+    }
+  },
+  methods: {
+    async fetchData() {
+      this.loading = true;
+      this.error = null;
+      try {
+        await this.$store.dispatch('roles/fetchRoles');
+        await this.$store.dispatch('users/fetchUserById', {id: this.userId});
+      } catch (err) {
+        this.error = 'Failed to load roles or user information.';
+      } finally {
+        this.loading = false;
+      }
+    },
+    async addRoleToUser(roleId) {
+      this.loading = true;
+      this.successMessage = null;
+      this.error = null;
+      try {
+        // Send the request to add the role to the user
+        await axios.post(
+            serverUrl,
+            {
+              userId: parseInt(this.userId),
+              roleId
+            },
+            {
+              headers: {
+                Authorization: `${this.$store.getters['auth/getPrefix']} ${this.$store.getters['auth/getToken']}`,
+              },
+            }
+        );
+
+        // Find the role name from the roles array
+        const role = this.roles.find(r => r.id === roleId);
+        if (role) {
+          this.successMessage = `User ${this.user.username} added to role ${role.name} successfully!`;
+        } else {
+          this.successMessage = 'Role added successfully, but role name could not be found.';
+        }
+      } catch (err) {
+        // Improved error handling with more specific messages
+        this.error =  'Failed to add role to user.';
+      } finally {
+        this.loading = false;
+      }
+    }
+  },
+  created() {
+    this.fetchData();
+  }
+};
+</script>
+
+<style scoped>
+.list {
+  list-style-type: none;
+  padding: 0;
+}
+
+.box {
+  width: 200px;
+}
+
+.notification-width{
+  width: 300px;
+}
+</style>
---
+++ client/src/pages/user-roles/AddUserToRole.vue
@@ -0,0 +1,99 @@
+<template>
+  <div class="container">
+    <h2 class="title is-4">Add User to Role</h2>
+    <h4 class="subtitle">Select a user to add to the role</h4>
+    <h4>Role Name: {{ role.name }}</h4>
+    <h4>Role Id: {{ role.id }}</h4>
+    <div class="notification is-info" v-if="loading">
+      Loading users...
+    </div>
+
+    <ul class="list">
+      <li v-for="user in users" :key="user.id" class="m-4">
+        <button class="button is-primary box" @click="addUserToRole(user.id)">
+          {{ user.username }} (ID: {{ user.id }})
+        </button>
+      </li>
+    </ul>
+
+    <div class="notification is-danger" v-if="error">{{ error }}</div>
+    <div class="notification is-success" v-if="successMessage">{{ successMessage }}</div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+const serverUrl = import.meta.env.VITE_SERVER_URL + "/user-roles";
+
+export default {
+  props: ['roleId'],
+  data() {
+    return {
+      loading: false,
+      error: null,
+      successMessage: null
+    };
+  },
+  computed: {
+    users() {
+      return this.$store.getters['users/allUsers'];
+    },
+    role() {
+      return this.$store.getters['roles/currentRole'];
+    }
+  },
+  methods: {
+    async fetchData() {
+      this.loading = true;
+      this.error = null;
+      try {
+        await this.$store.dispatch('users/fetchUsers');
+        await this.$store.dispatch('roles/fetchRoleById', {id: this.roleId});
+      } catch (err) {
+        this.error = 'Failed to load users';
+      } finally {
+        this.loading = false;
+      }
+    },
+    async addUserToRole(userId) {
+      this.loading = true;
+      this.successMessage = null;
+      this.error = null;
+      try {
+        await axios.post(
+            serverUrl,
+            {
+              userId,
+              roleId: parseInt(this.roleId)
+            },
+            {
+              headers: {
+                Authorization: `${this.$store.getters['auth/getPrefix']} ${this.$store.getters['auth/getToken']}`,
+              },
+            }
+        );
+        this.successMessage = `User ${userId} added to role ${this.roleId} successfully!`;
+      } catch (err) {
+        this.error = 'Failed to add user to role.';
+      } finally {
+        this.loading = false;
+      }
+    }
+  },
+  created() {
+    this.fetchData();
+  }
+};
+</script>
+
+<style scoped>
+.list {
+  list-style-type: none;
+  padding: 0;
+}
+
+.box {
+  width: 200px;
+}
+</style>
